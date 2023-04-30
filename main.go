406dffc49dea38e4c549e33acd74a56011a85212
yuuki-tsujimura <snow1218kei@gmail.com>
2023-04-30 12:43:23 +0900

    Encode example

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,29 +11,29 @@ type person struct {
 }
 
 func main() {
-	p1 := person{
-		First: "Janny",
-	}
+	// p1 := person{
+	// 	First: "Janny",
+	// }
 
-	p2 := person{
-		First: "James",
-	}
+	// p2 := person{
+	// 	First: "James",
+	// }
 
-	xp := []person{p1, p2}
+	// xp := []person{p1, p2}
 
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("PRINT JSON", string(bs))
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("PRINT JSON", string(bs))
 
-	xp2 := []person{}
+	// xp2 := []person{}
 
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("back into a Go data structure", xp2)
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("back into a Go data structure", xp2)
 
 	http.HandleFunc("/encode", foo)
 	http.HandleFunc("/decode", bar)
@@ -42,7 +41,14 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
+	p1 := person{
+		First: "Jenny",
+	}
 
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encode bad data", err)
+	}
 }
 
 func bar(w http.ResponseWriter, req *http.Request) {
