d854e30d474945b605ff76919bf8a1bd71dc58a4
yuuki-tsujimura <snow1218kei@gmail.com>
2023-04-30 16:34:05 +0900

    hashPassword

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/golang-arch
 
 go 1.19
+
+require golang.org/x/crypto v0.8.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-		fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:password")))
+		password := "123456"
+		hashedPassword, err := hashPassword(password)
+		if err != nil {
+				panic(err)
+		}
+
+		err = comparePassword(password, hashedPassword)
+		if err != nil {
+				log.Fatalln("Not logged in")
+		}
+		fmt.Println("Logged in")
+}
+
+func hashPassword(password string) ([]byte, error) {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+				return nil, fmt.Errorf("Error while generationg bcrypt hash from password: %w", err)
+		}
+		return hashedPassword, nil
+}
+
+func comparePassword(password string, hashedPassword []byte) error {
+		err := bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+		if err !=nil {
+				return fmt.Errorf("Invalid password: %w", err)
+		}
+		return nil
 }
